8c55a061db453ad1fe620b3e9cbdeb035636cbca
Limber <it@limberenergy.com>
2024-12-24 15:20:19 +0530

    water

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ hungry=input("are you hungry")
 if hungry=="yes":
     print("eat pizza")
 else:
+    thirsty=input("are you thirst?")
+    if thirsty=="yes":
+        print("drink water")
+    else:
+        print("drink coffee")    
+
     print("kuch nii")
