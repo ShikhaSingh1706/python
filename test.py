7fbb0cac04812b079c6476a350794cb9a1363dca
Limber <it@limberenergy.com>
2024-12-27 12:41:21 +0530

    all error removed

--- test.py
+++ test.py
@@ -5,15 +5,12 @@ if hungry=="yes":
 else:
     thirsty=input("are you thirst?")
     if thirsty=="yes":
-<<<<<<< HEAD
+
         print("drink water")
     else:
         print("do your homework")    
-=======
-        print("yes i am thirsty and i need water to drink!")
-    else:
-        print("dom your homework")    
+
+       
 
     
 #thirsty branch
->>>>>>> thirsty
